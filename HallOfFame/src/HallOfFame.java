762627a7f8a9cda72210c4e0f18d0ab120dac665
drosselloGH <danielrossellooliver@gmail.com>
2023-05-20 22:01:43 +0200

    revisar metodos

--- HallOfFame/src/HallOfFame.java
+++ HallOfFame/src/HallOfFame.java
@@ -2,10 +2,12 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,6 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 
 public class HallOfFame extends JFrame {
+
+	private JList<String> lstJugadores;
+
 	public HallOfFame() {
 		super("Puntuaciones");
 		setSize(330, 500);
@@ -21,7 +26,7 @@ public class HallOfFame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		JLabel lblInicio = new JLabel("***Hall of Fame***");
 
-		JList<String> lstJugadores = new JList<>();
+		lstJugadores = new JList<>();
 		JScrollPane srcPanel = new JScrollPane(lstJugadores);
 
 		srcPanel.setPreferredSize(new Dimension(300, 400));
@@ -60,6 +65,7 @@ public class HallOfFame extends JFrame {
 
 	public static void main(String[] args) {
 		cargarJugadores();
+
 	}
 
 	/**
@@ -67,46 +73,72 @@ public class HallOfFame extends JFrame {
 	 * listener del botón Nuevo.
 	 */
 	private void nuevoJugador() {
-		String nombre = JOptionPane.showInputDialog("¿Nombre del jugador?");
-		String puntuacion = JOptionPane.showInputDialog("¿Puntuación del jugador?");
+		leerDatosJugador();
+
 	}
 
+	
+					/*SE SUPONE QUE FUNCIONA*/
+	
+	
 	/**
 	 * Pide al usuario los datos de un nuevo jugador y devuelve un objeto Jugador.
 	 *
 	 * @return Un objeto Jugador con los datos leídos, o null si los datos no son
 	 *         válidos.
 	 */
-//	private Jugador leerDatosJugador() {
-//	}
-//
-//	/**
-//	 * Guarda la información de un jugador en el archivo de disco, en formato csv.
-//	 *
-//	 * @param jugador Objeto Jugador con el nombre y la puntuación a añadir.
-//	 */
-//
-//	public void guardarJugador(Jugador jugador) {
-//	}
-//
-//	/**
-//	 * Lee los datos sobre nombres y puntuaciones de jugadores. Los añade a la lista
-//	 * que se muestra en la ventana.
-//	 */
+	private Jugador leerDatosJugador() {
+		String nombre = JOptionPane.showInputDialog("¿Nombre del jugador?");
+		String puntuacion = JOptionPane.showInputDialog("¿Puntuación del jugador?");
+		Jugador jugador = new Jugador();
+		jugador.setNombre(nombre);
+		jugador.setPuntuacion((Integer.parseInt(puntuacion)));
+		return jugador;
+	}
+
+	/**
+	 * Guarda la información de un jugador en el archivo de disco, en formato csv.
+	 *
+	 * @param jugador Objeto Jugador con el nombre y la puntuación a añadir.
+	 */
+
+	public void guardarJugador(Jugador jugador) {
+		try {
+			FileWriter archivo = new FileWriter("fichero/jugadores.txt");
+			BufferedWriter buffer = new BufferedWriter(archivo);
+			
+			
+			
+		} catch (IOException e) {
+			System.out.println("No se pudo guardar el archivo");
+		}
+	}
+
+	
+							/*FUNCIONA, CREO*/
+	
+	
+	/**
+	 * Lee los datos sobre nombres y puntuaciones de jugadores. Los añade a la lista
+	 * que se muestra en la ventana.
+	 */
 	public static void cargarJugadores() {
 		try {
-			FileReader archvivo = new FileReader("/ficheros/jugadores.txt");
+			FileReader archvivo = new FileReader("ficheros/jugadores.txt");
+			new HallOfFame();
 		} catch (FileNotFoundException e) {
 			JOptionPane.showMessageDialog(null,
 					"No se han encontrado datos guardados de jugadores. Se creará un archivo nuevo");
 			try {
-				FileWriter archivo = new FileWriter("/ficheros/jugadores.txt", true);
+				FileWriter archivo = new FileWriter("ficheros/jugadores.txt", true);
 			} catch (IOException e1) {
 				JOptionPane.showMessageDialog(null, "No se ha podido crear el archivo");
 			}
 		}
 	}
 
+								/*REVISAR*/
+	
 	/**
 	 * Borra el contenido del archivo de jugadores, dejándolo en blanco. Y la lista
 	 * de jugadores en la ventana. Este método es llamado desde el listener del
@@ -118,6 +150,16 @@ public class HallOfFame extends JFrame {
 
 		if (borrar == 1) {
 			JOptionPane.showMessageDialog(null, "Operación cancelada. No se ha borrado ningún dato.");
+		} else {
+			try {
+				FileWriter archivo = new FileWriter("ficheros/jugadores.txt");
+				BufferedWriter buffer = new BufferedWriter(archivo);
+
+				buffer.write("");
+
+			} catch (IOException e) {
+				System.out.println("No se pudo borrar los datos");
+			}
 		}
 	}
 }
---
+++ HallOfFame/src/Jugador.java
@@ -0,0 +1,23 @@
+
+public class Jugador {
+
+	private String nombre;
+	private int puntuacion;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+}
